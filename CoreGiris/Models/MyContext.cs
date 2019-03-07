1e9e3dfed4e061572c2adb92ea4a7c346ec8306d
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-03-07 11:58:26 +0300

    Core veritabanı oluşturulup kategori eklendi.

---
+++ CoreGiris/Models/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreGiris.Models
+{
+    public class MyContext:DbContext
+    {
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;DataBase=UrunYonetimiDb;Trusted_connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(x => x.UnitPrice).HasColumnType("decimal(7,2)");
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
